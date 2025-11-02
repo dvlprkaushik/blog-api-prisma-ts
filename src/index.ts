d96f1d1c9ad5d76d688c9157b5e31f6110aa5ee4
kaushik <dvlprkaushik@gmail.com>
2025-11-02 13:41:16 +0530

    feat(server): initialize Express server with health route

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import chalk from "chalk";
+import dotenv from "dotenv";
+
+dotenv.config({ quiet: true });
+
+const app = express();
+
+// Middleware
+app.use(express.json({ limit: "5mb" }));
+
+// Health check route
+app.get("/", async (_req: Request, res: Response) => {
+  res.status(200).json({ message: "Blog API is running 🚀" });
+});
+
+// Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(chalk.bold.magentaBright(`✅ Server started on http://localhost:${PORT}`));
+});
