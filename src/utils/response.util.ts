4af2e247ea1108f7a4868de6ae6539799e33723d
kaushik <dvlprkaushik@gmail.com>
2025-11-02 17:36:42 +0530

    feat: minor addon of an response interface

--- src/utils/response.util.ts
+++ src/utils/response.util.ts
@@ -1,12 +1,18 @@
 import { Response } from "express";
 
+interface ApiResponse<T> {
+  success: boolean;
+  message: string;
+  data?: T;
+}
+
 export const sendResponse = <T>(
   res: Response,
   statusCode: number,
   success: boolean,
   message: string,
   data?: T
-): Response => {
+): Response<ApiResponse<T>> => {
   return res.status(statusCode).json({
     success,
     message,
